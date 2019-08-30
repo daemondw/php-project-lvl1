50993e5587274a2a53c25c2b8baa10207ed18980
Dima Antonenko <daemondw@gmail.com>
2019-08-30 19:32:15 +0200

    checking step 8

---
+++ src/games/progres.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BrainGames\Games\Progres;
+
+use function BrainGames\Game\goGame;
+
+const EXPLANATION = 'What number is missing in the progression?';
+
+function goProg()
+{
+    $createData = function () {
+        $randNum = rand(0, 9);
+        $arr = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23,];
+        $rightAnswer = $arr[$randNum];
+        $arr[$randNum] = '..';
+
+        $gues = "$arr";
+       
+        return [$rightAnswer, $gues];
+    };
+    goGame($createData, EXPLANATION);
+}
+
